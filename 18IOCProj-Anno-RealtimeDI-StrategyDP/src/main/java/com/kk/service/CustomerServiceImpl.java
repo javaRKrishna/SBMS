18cdd0f862f3541b3172491f504828c01ad1a7e2
RKrishna <RKrishna@DESKTOP-JEOJ4RJ>
2021-07-14 18:16:53 +0530

    RealTimeDI-Annotation Update

---
+++ 18IOCProj-Anno-RealtimeDI-StrategyDP/src/main/java/com/kk/service/CustomerServiceImpl.java
@@ -0,0 +1,34 @@
+package com.kk.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.kk.bo.CustomerBO;
+import com.kk.dao.ICustomerDAO;
+import com.kk.dto.CustomerDTO;
+
+@Service
+public class CustomerServiceImpl implements ICustomerService{
+	@Autowired
+	@Qualifier("dao")
+	private ICustomerDAO dao;
+	
+	@Override
+	public String clcSimpleIntrAmou(CustomerDTO dto) throws Exception {
+		//calculate SI ammount
+		float intAmt=(dto.getpAmt()*dto.getRate()*dto.getTime())/100.0f;
+		//create BO class obj having persistance data
+		CustomerBO bo=new CustomerBO();
+		bo.setCustName(dto.getCustName());
+		bo.setCustAddrs(dto.getCustAddrs());
+		bo.setpAmt(dto.getpAmt());
+		bo.setRate(dto.getRate());
+		bo.setTime(dto.getTime());
+		bo.setIntAmt(intAmt);
+		//use DAO
+		int count =dao.insert(bo);
+		return count==1?" Customer registred Successfully ---> SI Amount:: "+intAmt:" Customer not registred ";
+	}
+
+}
